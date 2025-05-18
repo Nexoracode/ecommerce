58af798c328a14538b0a39f356e3cd821eb64d57
Mohammadreza Maddahi <mrmahan532@gmail.com>
2025-05-18 13:51:34 +0330

    create user entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
-  imports: [AppConfigModule],
+  imports: [AppConfigModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
