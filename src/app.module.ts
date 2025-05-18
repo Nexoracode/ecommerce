b1fd5aaf928921f7f26db22127c391dbbb75012a
Nexoracode <maddahi698@gmail.com>
2025-05-18 20:49:30 +0430

    added resurce auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { AppConfigModule } from './config/config.module';
 import { UserModule } from './modules/user/user.module';
 import { AddressModule } from './modules/address/address.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [AppConfigModule, UserModule, AddressModule],
+  imports: [AppConfigModule, UserModule, AddressModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
