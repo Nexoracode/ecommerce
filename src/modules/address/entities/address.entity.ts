83d0e0933d247268aff3dbf51b1856e09686689a
Nexoracode <maddahi698@gmail.com>
2025-05-18 20:48:01 +0430

    added entity user and address

--- src/modules/address/entities/address.entity.ts
+++ src/modules/address/entities/address.entity.ts
@@ -1,1 +1,27 @@
-export class Address {}
+import { User } from "src/modules/user/entities/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Address {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column()
+    city: string;
+
+    @Column()
+    state: string;
+
+    @Column()
+    address: string;
+
+    @Column({ name: 'postal_code' })
+    postalCode: string;
+
+    @Column({ name: 'is_primary', default: false })
+    isPrimary: boolean;
+
+    @ManyToOne(() => User, (user) => user.addresses, { onDelete: 'CASCADE' })
+    @JoinColumn({ name: 'user_id' })
+    user: User;
+}
--- src/modules/user/entities/user.entity.ts
+++ src/modules/user/entities/user.entity.ts
@@ -1,1 +1,47 @@
-export class User { }
+import { Address } from "src/modules/address/entities/address.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+enum UserRole {
+    ADMIN = 'admin',
+    USER = 'user',
+}
+
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: string;
+
+    @Column({ name: 'first_name', type: 'varchar', length: 100 })
+    firstName: string;
+
+    @Column({ name: 'last_name', type: 'varchar', length: 100 })
+    lastName: string;
+
+    @Column({ name: 'phone', type: 'varchar', length: 11, unique: true })
+    phone: string;
+
+    @Column({ name: 'email', type: 'varchar', length: 100, unique: true })
+    email: string;
+
+    @Column({ name: 'password', type: 'varchar', length: 100 })
+    password: string;
+
+    @Column({ name: 'role', type: 'enum', enum: UserRole, default: UserRole.USER })
+    role: UserRole;
+
+    @OneToMany(() => Address, (address) => address.user, { cascade: true })
+    addresses: Address[];
+
+    // @OneToMany(() => Order, (order) => order.user, { cascade : true })
+    // orders : Order[];
+
+    // @OneToMany(() => Cart, (cart) => cart.user, { cascade : true })
+    // cart : Cart[];
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+
+}
