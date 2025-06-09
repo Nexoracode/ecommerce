0543717915cc0db5fc8bcb7842df23ec69ba87c4
Nexoracode <maddahi698@gmail.com>
2025-06-09 14:23:23 +0330

    set permistion address method

--- src/modules/address/address.controller.ts
+++ src/modules/address/address.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, UseGuards } from '@nestjs/common';
 import { AddressService } from './address.service';
 import { CreateAddressDto } from './dto/create-address.dto';
 import { UpdateAddressDto } from './dto/update-address.dto';
+import { AccessGuard } from 'src/common/guard/access.guard';
+import { RoleGuard } from 'src/common/guard/role.guard';
+import { OwnerGuard } from 'src/common/guard/owner.guard';
 
+@UseGuards(AccessGuard, OwnerGuard)
 @Controller('addresses')
 export class AddressController {
     constructor(
